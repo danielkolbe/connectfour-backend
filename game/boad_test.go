c38703966a4b18c3f33bdd5711c1e21e95ab8c4a
Daniel Kolbe <daniel-kolbe@web.de>
2020-06-12 21:15:13 +0200

    Added first test for addChip method

--- game/boad_test.go
+++ game/boad_test.go
@@ -6,12 +6,23 @@ import (
 
 func TestNewBoard(t *testing.T) {
 	b := NewBoard()
-	b.AddChip(1, RED)
 	for i, row := range b {
 		for j, color := range row {
 			if NONE != color {
 				t.Errorf("A newly created board should be blank but the color of field %v,%v was %v", i, j, Color(1))
 			}
 		}
+	}
+}
+
+func TestAddChip(t *testing.T) {
+	b := NewBoard()
+	b.AddChip(4,2)
+	b.AddChip(4,1)
+	if RED != b[5][4] {
+		t.Errorf("Color of field 6,4 of the board should be red but was %v", Color(b[5][4]))
+	}
+	if BLUE != b[4][4] {
+		t.Errorf("Color of field 6,4 of the board should be blue but was %v", Color(b[4][4]))
 	}
 }
