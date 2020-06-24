d18a54990166a5d72865bcca8acf361a5b2fd63d
Daniel Kolbe <daniel-kolbe@web.de>
2020-06-24 12:04:20 +0200

    Added error case handling.

--- app/main.go
+++ app/main.go
@@ -7,8 +7,11 @@ import (
 
 func main() {
 	board := game.NewBoard()
-	board.AddChip(2, game.RED)
-	for _,row := range board {
+	err := board.AddChip(2, -1)
+	if nil != err {
+		fmt.Println(err)
+	}
+	for _, row := range board {
 		fmt.Printf("%v\n", row)
 	}
 }
--- game/board.go
+++ game/board.go
@@ -1,5 +1,9 @@
 package game
 
+import (
+	"fmt"
+)
+
 type Color int
 type Board [nRows][nCol] Color
 const nRows int = 6
@@ -11,15 +15,26 @@ const(
 	RED
 )
 
+func (color Color) String() string {
+	if(color > 2 || color < 0) {
+		return "Unknown"
+	}
+	return []string{"NONE", "BLUE", "RED"}[color]
+}
+
 func NewBoard() Board {
 	return [nRows][nCol]Color{}
 }
 
-func (b *Board) AddChip(column int, color Color){
+func (b *Board) AddChip(column int, color Color) error{
+	if !(BLUE == color || RED == color) {
+		return fmt.Errorf("Expected chip color to be either %v(%d) or %v(%d) but received %v(%d).", BLUE, BLUE, RED, RED, color, color)
+	}
 	for i := len(b)-1; i>=0; i-- {
 		if(NONE == b[i][column]) {
 			b[i][column] = color
 			break
 		}
 	}
+	return nil
 }
