46befed16d5172a3813d52b25d64acf9fa9141af
Daniel Kolbe <daniel-kolbe@web.de>
2020-06-26 09:08:29 +0200

    Renamed color type to make it package private.

--- api/handlers.go
+++ api/handlers.go
@@ -15,8 +15,11 @@ func Turn(w http.ResponseWriter, req *http.Request) {
 		w.WriteHeader(http.StatusBadRequest)
 		fmt.Fprint(w, "Please provide a query parameter 'column' as integer greater or equal 0")
 	}
-	b.AddChip(c)
-	fmt.Print("\n")
+	err = b.AddChip(c)
+	if(nil != err) {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, err)
+	}
 	for _, row := range b.Fields {
 		fmt.Printf("%v\n", row)
 	}
--- game/board.go
+++ game/board.go
@@ -4,35 +4,36 @@ import (
 	"fmt"
 )
 
-type Color int
+type color int
 type Board struct {
-	Fields [nRows][nCol] Color
-	nextColor Color
+	Fields [nRows][nCol] color
+	nextColor color
 }	
+
 const nRows int = 6
 const nCol int = 7
 
 const(
-	NONE Color = iota
+	NONE color = iota
 	BLUE
 	RED
 )
 
-func (color Color) String() string {
-	if(color > 2 || color < 0) {
+func (c color) String() string {
+	if(c > 2 || c < 0) {
 		return "Unknown"
 	}
-	return []string{"NONE", "BLUE", "RED"}[color]
+	return []string{"NONE", "BLUE", "RED"}[c]
 }
 
 func NewBoard() Board {
-	return Board{Fields: [nRows][nCol]Color{}, nextColor: RED}
+	return Board{Fields: [nRows][nCol]color{}, nextColor: RED}
 }
 
 func (b *Board) AddChip(column int) error {
 	
 	if NONE != b.Fields[0][column]  {
-		return fmt.Errorf("Selected column %v is full", column)
+		return fmt.Errorf("Column %v is full", column)
 	}
 	
 	for row := len(b.Fields)-1; row>=0; row-- {
