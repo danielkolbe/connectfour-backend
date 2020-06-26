8d7015576577f0c24db3123daed5ac054c6a42a5
Daniel Kolbe <daniel-kolbe@web.de>
2020-06-26 23:12:49 +0200

    Added unit tests for game.Turn function

--- game/game_test.go
+++ game/game_test.go
@@ -5,8 +5,22 @@ import (
 )
 
 func TestTurn(t *testing.T) {
-	var gameDb = map[string]board{}
-    a,b := gameDb["fsdfdf"]
-	t.Log(a)
-	t.Log(b)
+	if 0 != len(gameDb) {
+		t.Errorf("Initial size of game database should be zero but was: %v", len(gameDb))
+	}
+	Turn(3, "id_1")	
+	
+	if 1 != len(gameDb) {
+		t.Errorf("Size of game database should be 1 but was: %v", len(gameDb))
+	}
+	Turn(3, "id_1")
+	
+	if 1 != len(gameDb) {
+		t.Errorf("Size of game database should be 1 but was: %v", len(gameDb))
+	}
+	Turn(3, "id_2")
+	
+	if 2 != len(gameDb) {
+		t.Errorf("Size of game database should be 2 but was: %v", len(gameDb))
+	}
 }
