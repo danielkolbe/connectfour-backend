053568ba3e65e4d7b3796ae7203acb4f8070fb0f
Daniel Kolbe <daniel-kolbe@web.de>
2020-07-10 15:50:51 +0200

    Added tests for api service.

---
+++ api/service_test.go
@@ -0,0 +1,29 @@
+package api
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"regexp"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestGameID(t *testing.T) {
+	// Arrange
+	cookie := &http.Cookie{Name: "gameID", Value: "324234-555"}
+	req, _ := http.NewRequest("", "", nil)
+    req.AddCookie(cookie)
+	rr := httptest.NewRecorder()
+	// Act
+	id := gameID(rr, req)
+	// Assert
+	require.Equal(t, "324234-555", id, "Should return gameID extracted from cookie.")
+	// Arrange 
+	req, _ = http.NewRequest("", "", nil)
+	r := regexp.MustCompile("^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-4[a-fA-F0-9]{3}-[8|9|aA|bB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}$")
+	// Act
+	id = gameID(rr, req)
+	// Assert
+	require.Equal(t, true, r.MatchString(id), "Should return a genrated uuid if no cookie given.")
+}
